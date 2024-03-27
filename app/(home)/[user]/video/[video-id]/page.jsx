55bf322c6f542bb071b9fb9fbda4b780621a6617
“hylDDD666” <2579761921@qq.com>
2024-03-28 01:23:52 +0800

    实现了详情页响应式布局

--- app/(home)/[user]/video/[video-id]/page.jsx
+++ app/(home)/[user]/video/[video-id]/page.jsx
@@ -1,14 +1,17 @@
 'use client'
+import VideoDetail from '@/app/ui/detail/video-detail'
 import { Col, Row } from 'antd'
 import React from 'react'
 
 export default function page() {
   return (
     <>
-      <div className="fixed w-screen h-screen left-1 top-1 z-50">
+      <div className="fixed w-screen h-screen left-0 top-0 z-50">
         <div className="flex flex-nowrap h-full overflow-hidden">
-          <div className="min-w-128 h-full bg-white flex-auto">video</div>
-          <div className="h-full w-[544px] bg-slate-300">detail</div>
+          <div className="h-full bg-white  grow basis-[500px] shrink-0">
+            <VideoDetail></VideoDetail>
+          </div>
+            <div className="h-full basis-[544px] bg-slate-300">detail</div>
         </div>
       </div>
     </>
---
+++ app/ui/detail/video-detail.jsx
@@ -0,0 +1,287 @@
+'use client'
+import {
+  ArrowUpOutlined,
+  CloseCircleOutlined,
+  DislikeOutlined,
+  EllipsisOutlined,
+  FlagOutlined,
+  MutedOutlined,
+  PauseOutlined,
+  PlayCircleFilled,
+  SoundOutlined
+} from '@ant-design/icons'
+import { Button, Col, Popover, Row, Slider, Tooltip } from 'antd'
+import React, { Suspense, lazy, useEffect, useRef, useState } from 'react'
+import { useHomeStore } from '../../stores/homeStore'
+import ReactPlayer from 'react-player'
+import { useRouter } from 'next/navigation'
+
+export default function VideoDetail(props) {
+  const [domLoaded, setDomLoaded] = useState(false)
+  const [isPlay, setIsPlay] = useState(true)
+  const [volume, setVolume] = useState(20)
+  const [isMuted, setIsMuted] = useState(true)
+  const [duration, setDuration] = useState(0)
+  const [sliderValue, setSliderValue] = useState(0)
+  const [show, setShow] = useState(false)
+  const router = useRouter()
+  const playerRef = useRef()
+
+  let type = 'col'
+  let isAutoRoll = false
+
+  useEffect(() => {
+    setDomLoaded(true)
+  }, [])
+  const onChange = (newValue) => {
+    setSliderValue(newValue)
+    playerRef.current.seekTo(newValue / 100)
+  }
+
+  const disLikeItem = useHomeStore((state) => state.disLikeItem)
+  // const isAutoRoll = useHomeStore((state) => state.isAutoRoll)
+  // const toggleAutoRoll = useHomeStore((state) => state.toggleAutoRoll)
+  // const playItemById = useHomeStore((state) => state.playItemById)
+  // const pauseItemById = useHomeStore((state) => state.pauseItemById)
+  // const { id, isPlay,user } = props
+
+  const handleDuration = (value) => {
+    setDuration(value)
+  }
+  const volumeChange = (newValue) => {
+    if (newValue > 0) setIsMuted(false)
+    setVolume(newValue)
+  }
+  const togglePlaying = (e) => {
+    e.stopPropagation()
+    setIsPlay((pre) => !pre)
+  }
+  // const toggleRoll = (e) => {
+  //   e.stopPropagation()
+  //   toggleAutoRoll()
+  // }
+  const handllePlay = (played) => {
+    setSliderValue((played.playedSeconds / duration) * 100)
+  }
+  const handleMute = (e) => {
+    e.stopPropagation()
+    setIsMuted((pre) => !pre)
+  }
+  const handleMouseEnter = () => {
+    setShow(true)
+  }
+  const handleMouseLeave = () => {
+    setShow(false)
+  }
+  const handleDislike = (e) => {
+    e.stopPropagation()
+    disLikeItem(id)
+  }
+  // const handleAutoPlay = () => {
+  //   if (isPlay) {
+  //     props.scrollNext()
+  //   }
+  // }
+  // const videoClickHandler = () => {
+  //   router.push(`/${user.userName}/video/${id}`)
+  // }
+  return (
+    <>
+      {domLoaded && (
+        <div
+          className={`h-full w-full  relative   overflow-hidden bg-black z-0`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onClick={togglePlaying}
+        >
+          <div
+            className={`w-full h-full absolute bg-transparent z-10 opacity-0 ${
+              show ? 'opacity-100' : ''
+            } transition-opacity`}
+          >
+            <div
+              className={'absolute top-5 right-5 text-white '}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <Popover
+                content={
+                  <div > 
+                    <div>
+                      <Button
+                        style={{
+                          fontWeight: 'bold',
+                          border: 0,
+                        }}
+                        icon={<DislikeOutlined />}
+                        onClick={handleDislike}
+                      >
+                        不感兴趣
+                      </Button>
+                    </div>
+                    <div>
+                      <Button
+                        style={{
+                          fontWeight: 'bold',
+                          border: 0,
+                        }}
+                        icon={<FlagOutlined />}
+                      >
+                        举报
+                      </Button>
+                    </div>
+                  </div>
+                }
+                placement="bottomRight"
+                arrow={false}
+                className="rounded"
+              >
+                <EllipsisOutlined className="text-3xl !font-bold hover:cursor-pointer " />
+              </Popover>
+            </div>
+            <div
+              className={
+                'absolute bottom-0 w-full h-[60px] text-white px-[10px] bg-gradient-to-t from-slate-800 '
+              }
+              onClick={(e) => e.stopPropagation()}
+            >
+              <Row justify={'space-between'}>
+                <Col span={'auto'}>
+                  <Button
+                    icon={
+                      isPlay ? (
+                        <PauseOutlined className={'!text-xl'} />
+                      ) : (
+                        <PlayCircleFilled className={'!text-xl'} />
+                      )
+                    }
+                    style={{
+                      border: 0,
+                      padding: 0,
+                      backgroundColor: 'transparent',
+                      color: 'white'
+                    }}
+                    onClick={togglePlaying}
+                  ></Button>
+                </Col>
+                <Col span={'60px'}>
+                  <Tooltip
+                    placement="top"
+                    arrow={false}
+                    title={isAutoRoll ? '自动滚动已开启' : '自动滚动已关闭'}
+                    color="rgb(54,54,54)"
+                    overlayStyle={{ fontSize: 'small', color: 'white' }}
+                  >
+                    <Button
+                      icon={
+                        isAutoRoll ? (
+                          <ArrowUpOutlined className={'!text-xl'} />
+                        ) : (
+                          <CloseCircleOutlined className={'!text-xl'} />
+                        )
+                      }
+                      // onClick={toggleRoll}
+                      style={{
+                        border: 0,
+                        padding: 0,
+                        backgroundColor: 'transparent',
+                        color: 'white',
+                        width: '28px'
+                      }}
+                    ></Button>
+                  </Tooltip>
+                  <Popover
+                    placement="top"
+                    arrow={false}
+                    color="transparent"
+                    content={
+                      <div className="h-[60px]">
+                        <Slider
+                          vertical
+                          defaultValue={30}
+                          style={{ margin: 0 }}
+                          value={isMuted ? 0 : volume}
+                          onChange={volumeChange}
+                        />
+                      </div>
+                    }
+                  >
+                    <Button
+                      icon={
+                        volume === 0 || isMuted ? (
+                          <MutedOutlined className={'!text-xl'} />
+                        ) : (
+                          <SoundOutlined className={'!text-xl'} />
+                        )
+                      }
+                      style={{
+                        border: 0,
+                        padding: 0,
+                        backgroundColor: 'transparent',
+                        color: 'white',
+                        width: '28px'
+                      }}
+                      onClick={handleMute}
+                    ></Button>
+                  </Popover>
+                </Col>
+              </Row>
+              <Row>
+                <Col flex={'auto'}>
+                  <Slider
+                    min={0}
+                    max={100}
+                    onChange={onChange}
+                    value={sliderValue}
+                    style={{ margin: 0, marginTop: '3px' }}
+                    tooltip={{
+                      open: false
+                    }}
+                  />
+                </Col>
+                <Col flex={'70px'} className="leading-3">
+                  <span className="text-xs ml-3">
+                    {' '}
+                    {Math.floor((sliderValue * duration) / 6000) < 10
+                      ? '0' + Math.floor((sliderValue * duration) / 6000)
+                      : Math.floor((sliderValue * duration) / 6000)}
+                    :
+                    {Math.floor(((sliderValue * duration) / 100) % 60) < 10
+                      ? '0' + Math.floor(((sliderValue * duration) / 100) % 60)
+                      : Math.floor(((sliderValue * duration) / 100) % 60)}
+                  </span>
+                  /
+                  <span className="text-xs ">
+                    {Math.floor(duration / 60) < 10
+                      ? '0' + Math.floor(duration / 60)
+                      : Math.floor(duration / 60)}
+                    :
+                    {Math.floor(duration % 60) < 10
+                      ? '0' + Math.floor(duration % 60)
+                      : Math.floor(duration % 60)}
+                  </span>
+                </Col>
+              </Row>
+            </div>
+          </div>
+          <div className="z-0 h-full">
+            <ReactPlayer
+              ref={playerRef}
+              playing={isPlay}
+              loop={!isAutoRoll}
+              volume={isMuted ? 0 : volume / 100}
+              width={'100%'}
+              height={'100%'}
+              url={
+                'https://player.vimeo.com/progressive_redirect/playback/917485262/rendition/360p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=bc021f21549b97299df9a8a3325e41939225fefe780309311704fa3caa3c71f7'
+              }
+              onDuration={handleDuration}
+              onProgress={handllePlay}
+              // onEnded={handleAutoPlay}
+              style={type === 'row' ? { position: 'absolute' } : {}}
+            ></ReactPlayer>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
